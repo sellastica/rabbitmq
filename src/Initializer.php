9d51f853fb8bc0374f1f864e0788c7cd67f1949a
Tomáš Krejčí <tomas@differencemakers.cz>
2018-01-02 16:06:22 +0100

    initializer

--- src/Initializer.php
+++ src/Initializer.php
@@ -3,34 +3,49 @@ namespace Sellastica\RabbitMQ;
 
 class Initializer
 {
-	/** @var \Sellastica\RabbitMQ\RabbitMQFactory */
-	private $rabbitMQFactory;
-	/** @var \Gamee\RabbitMQ\Connection\ConnectionFactory */
-	private $connectionFactory;
+	/** @var \Gamee\RabbitMQ\Connection\ConnectionsDataBag */
+	private $dataBag;
+	/** @var \Sellastica\Project\Model\SettingsAccessor */
+	private $settingsAccessor;
 
 
 	/**
-	 * @param \Sellastica\RabbitMQ\RabbitMQFactory $rabbitMQFactory
-	 * @param \Gamee\RabbitMQ\Connection\ConnectionFactory $connectionFactory
+	 * @param \Gamee\RabbitMQ\Connection\ConnectionsDataBag $dataBag
+	 * @param \Sellastica\Project\Model\SettingsAccessor $settingsAccessor
 	 */
 	public function __construct(
-		\Sellastica\RabbitMQ\RabbitMQFactory $rabbitMQFactory,
-		\Gamee\RabbitMQ\Connection\ConnectionFactory $connectionFactory
+		\Gamee\RabbitMQ\Connection\ConnectionsDataBag $dataBag,
+		\Sellastica\Project\Model\SettingsAccessor $settingsAccessor
 	)
 	{
-		$this->rabbitMQFactory = $rabbitMQFactory;
-		$this->connectionFactory = $connectionFactory;
+		$this->dataBag = $dataBag;
+		$this->settingsAccessor = $settingsAccessor;
 	}
 
 	public function initialize(): void
 	{
-		$connection = $this->connectionFactory->getConnection('default');
-		$apiClient = $this->rabbitMQFactory->create();
-		$apiClient->put("vhosts/$connection->vhost");
-		$apiClient->put("permissions/$connection->vhost/$connection->user", [
+		$settings = $this->settingsAccessor->get();
+		if ($settings->getSetting('rabbitmq.initialized')) {
+			return;
+		}
+
+		$credentials = (object)$this->dataBag->getDataBykey('default');
+		$apiClient = new RabbitMQ(
+			$credentials->host,
+			15672, //default Web UI and API port
+			$credentials->user,
+			$credentials->password,
+			'/'
+		);
+		//create vhost
+		$apiClient->put("vhosts/$credentials->vhost");
+		//assign user to vhost
+		$apiClient->put("permissions/$credentials->vhost/$credentials->user", [
 			'configure' => '.*',
 			'write' => '.*',
 			'read' => '.*',
 		]);
+		//save initialization
+		$settings->saveSettingValue('initialized', 'rabbitmq', 1);
 	}
 }
